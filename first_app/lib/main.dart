1e0a32e826475586298f3635d89fc1ad593df269
Moayadooh <moayadsalim97@gmail.com>
2023-10-05 22:04:29 +0400

    update

--- first_app/lib/main.dart
+++ first_app/lib/main.dart
@@ -8,12 +8,23 @@ void main() {
         //backgroundColor: Colors.green,
         body: Container(
           decoration: const BoxDecoration(
-              gradient: LinearGradient(colors: [
-            Color.fromARGB(255, 242, 11, 11),
-            Color.fromARGB(255, 5, 238, 25)
-          ])),
+            gradient: LinearGradient(
+              colors: [
+                Color.fromARGB(255, 242, 11, 11),
+                Color.fromARGB(255, 5, 238, 25)
+              ],
+              begin: Alignment.topLeft,
+              end: Alignment.bottomRight,
+            ),
+          ),
           child: const Center(
-            child: Text("My first Flutter app"),
+            child: Text(
+              "My first Flutter app",
+              style: TextStyle(
+                color: Colors.white,
+                fontSize: 28,
+              ),
+            ),
           ),
         ),
       ),
