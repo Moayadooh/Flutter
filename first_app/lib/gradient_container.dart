8dc7c41a55bdcb2cd2338260d501c492c9dfa3d6
Moayadooh <moayadsalim97@gmail.com>
2023-10-08 10:47:42 +0400

    adding images

--- first_app/lib/gradient_container.dart
+++ first_app/lib/gradient_container.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:first_app/styled_text.dart';
+//import 'package:first_app/styled_text.dart';
 
 //Alignment? startAlignment;
 //startAlignment = Alignment.topLeft;
@@ -11,6 +11,10 @@ const endAlignment = Alignment.bottomRight;
 class GradientContainer extends StatelessWidget {
   //GradientContainer({key}): super(key: key);
   const GradientContainer({super.key, required this.colors});
+
+  const GradientContainer.purple({super.key})
+      : colors = const [Colors.deepPurple, Colors.indigo];
+
   final List<Color> colors;
 
   @override
@@ -23,8 +27,11 @@ class GradientContainer extends StatelessWidget {
           end: endAlignment,
         ),
       ),
-      child: const Center(
-        child: StyledText("text"),
+      child: Center(
+        child: Image.asset(
+          'assets/images/dice-2.png',
+          width: 200,
+        ), //StyledText("text"),
       ),
     );
   }
