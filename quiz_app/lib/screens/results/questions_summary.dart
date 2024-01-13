9627c5d9405ed98eb49cce782ba17776110615d6
Moayadooh <moayadsalim97@gmail.com>
2024-01-13 12:08:21 +0400

    create my own folders

--- quiz_app/lib/screens/results/questions_summary.dart
+++ quiz_app/lib/screens/results/questions_summary.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:quiz_app/summary_item.dart';
+import 'package:quiz_app/screens/results/summary_item.dart';
 
 class QuestionsSummary extends StatelessWidget {
   const QuestionsSummary(this.summaryData, {super.key});
--- quiz_app/lib/screens/results/summary_item.dart
+++ quiz_app/lib/screens/results/summary_item.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
-import 'package:quiz_app/question_identifier.dart';
+import 'package:quiz_app/screens/results/question_identifier.dart';
 
 class SummaryItem extends StatelessWidget {
   const SummaryItem(this.itemData, {super.key});
