97c9e74fde7ff701d0da01ff37fb568ec09838fc
Moayadooh <moayadsalim97@gmail.com>
2024-01-19 00:17:29 +0400

    adding modal and input fields with date picker

--- expense_tracker/lib/main.dart
+++ expense_tracker/lib/main.dart
@@ -5,6 +5,7 @@ import 'package:expense_tracker/widgets/expenses.dart';
 void main() {
   runApp(
     const MaterialApp(
+      //theme: ThemeData(useMaterial3: true),
       home: Expenses(),
     ),
   );
