bb825e5f931a962897e79bbd4de75bec30b43021
Moayadooh <moayadsalim97@gmail.com>
2024-01-27 15:51:45 +0400

    Practise 2

--- practise_2/lib/my_form.dart
+++ practise_2/lib/my_form.dart
@@ -1,27 +1,27 @@
 import 'package:flutter/material.dart';
 
-class MyForm extends StatelessWidget {
+// class MyForm extends StatelessWidget {
+//   const MyForm({super.key});
+
+//   @override
+//   Widget build(BuildContext context) {
+//     return const MaterialApp(
+//       home: MyForm1(),
+//     );
+//   }
+// }
+
+class MyForm extends StatefulWidget {
   const MyForm({super.key});
 
   @override
-  Widget build(BuildContext context) {
-    return const MaterialApp(
-      home: MyForm1(),
-    );
-  }
-}
-
-class MyForm1 extends StatefulWidget {
-  const MyForm1({super.key});
-
-  @override
   //State<StatefulWidget> createState() {
-  State<MyForm1> createState() {
+  State<MyForm> createState() {
     return _MyForm();
   }
 }
 
-class _MyForm extends State<MyForm1> {
+class _MyForm extends State<MyForm> {
   final _titleController = TextEditingController();
 
   @override
@@ -32,16 +32,18 @@ class _MyForm extends State<MyForm1> {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text('Sample Code'),
-      ),
-      body: Center(
-        child: TextField(
-          controller: _titleController,
-          maxLength: 50,
-          decoration: const InputDecoration(
-            label: Text('Title'),
+    return MaterialApp(
+      home: Scaffold(
+        body: Center(
+          child: Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: TextField(
+              controller: _titleController,
+              maxLength: 50,
+              decoration: const InputDecoration(
+                label: Text('Title'),
+              ),
+            ),
           ),
         ),
       ),
