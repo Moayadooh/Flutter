2e4d829e80b535fa6620d61fe6e23676aa32a476
Moayadooh <moayadsalim97@gmail.com>
2024-01-28 22:20:47 +0400

    Practise 2 - text field and dialog

--- practise_2/lib/main.dart
+++ practise_2/lib/main.dart
@@ -2,5 +2,9 @@ import 'package:flutter/material.dart';
 import 'package:practise_2/my_form.dart';
 
 void main() {
-  runApp(const MyForm());
+  runApp(
+    const MaterialApp(
+      home: MyForm(),
+    ),
+  );
 }
--- practise_2/lib/my_form.dart
+++ practise_2/lib/my_form.dart
@@ -13,6 +13,24 @@ class MyForm extends StatefulWidget {
 class _MyForm extends State<MyForm> {
   final _titleController = TextEditingController();
 
+  void _showDialog() {
+    showDialog(
+      context: context,
+      builder: (ctx) => AlertDialog(
+        title: const Text('Dialog Title'),
+        content: Text(_titleController.text),
+        actions: [
+          TextButton(
+            onPressed: () {
+              Navigator.pop(ctx);
+            },
+            child: const Text('Done'),
+          )
+        ],
+      ),
+    );
+  }
+
   @override
   void dispose() {
     _titleController.dispose();
@@ -21,19 +39,27 @@ class _MyForm extends State<MyForm> {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      home: Scaffold(
-        body: Center(
-          child: Padding(
-            padding: const EdgeInsets.all(8.0),
-            child: TextField(
+    return Scaffold(
+      body: Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            TextField(
               controller: _titleController,
               maxLength: 50,
               decoration: const InputDecoration(
                 label: Text('Title'),
               ),
             ),
-          ),
+            ElevatedButton(
+              onPressed: () {
+                //print(_titleController.text);
+                _showDialog();
+              },
+              child: const Text('Save Expense'),
+            ),
+          ],
         ),
       ),
     );
