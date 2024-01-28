3e4e17b20560c0e8dc0b5fc328161e68049f8553
Moayadooh <moayadsalim97@gmail.com>
2024-01-28 07:53:16 +0400

    Practise 2 - wrappers

--- practise_2/lib/my_form.dart
+++ practise_2/lib/my_form.dart
@@ -1,16 +1,5 @@
 import 'package:flutter/material.dart';
 
-// class MyForm extends StatelessWidget {
-//   const MyForm({super.key});
-
-//   @override
-//   Widget build(BuildContext context) {
-//     return const MaterialApp(
-//       home: MyForm1(),
-//     );
-//   }
-// }
-
 class MyForm extends StatefulWidget {
   const MyForm({super.key});
 
