84c0d71b630e463bcc69aeefd023921ed9ac83e5
Moayadooh <moayadsalim97@gmail.com>
2024-02-02 09:40:02 +0400

    documentations

--- practise_2/lib/my_form.dart
+++ practise_2/lib/my_form.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:intl/intl.dart';
 
 class MyForm extends StatefulWidget {
   const MyForm({super.key});
@@ -11,45 +10,11 @@ class MyForm extends StatefulWidget {
   }
 }
 
-final formatter = DateFormat.yMd();
-
 class _MyForm extends State<MyForm> {
-  DateTime? _selectedDate;
-  void _presentDatePicker() async {
-    final now = DateTime.now();
-    final firstDate = DateTime(now.year - 1, now.month, now.day);
-    final pickedDate = await showDatePicker(
-      context: context,
-      initialDate: now,
-      firstDate: firstDate,
-      lastDate: now,
-    ); //.then((value) {
-    //},);
-    setState(() {
-      _selectedDate = pickedDate;
-    });
-  }
-
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        crossAxisAlignment: CrossAxisAlignment.center,
-        children: [
-          Text(
-            _selectedDate == null
-                ? 'No date selected'
-                : formatter.format(_selectedDate!),
-          ),
-          IconButton(
-            onPressed: _presentDatePicker,
-            icon: const Icon(
-              Icons.calendar_month,
-            ),
-          ),
-        ],
-      ),
-    );
+    return const Scaffold(
+        //body: ...
+        );
   }
 }
