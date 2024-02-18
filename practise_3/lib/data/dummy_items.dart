566138362069ace3c28e82dba550d35c0b4b4929
Moayadooh <moayadsalim97@gmail.com>
2024-02-18 07:09:25 +0400

    practise 3

---
+++ practise_3/lib/data/dummy_items.dart
@@ -0,0 +1,21 @@
+import 'package:practise_3/models/grocery_item.dart';
+import 'package:practise_3/data/categories.dart';
+import 'package:practise_3/models/category.dart';
+
+const groceryItems = [
+  GroceryItem(
+      id: 'a',
+      name: 'Milk',
+      quantity: 1,
+      category: categories[Categories.dairy]!),
+  GroceryItem(
+      id: 'b',
+      name: 'Bananas',
+      quantity: 5,
+      category: categories[Categories.fruit]!),
+  GroceryItem(
+      id: 'c',
+      name: 'Beef Steak',
+      quantity: 1,
+      category: categories[Categories.meat]!),
+];
